dd5b724e7bee12bff53253e1a166e22bec94bac4
TAMILSELVAM-A <tamilselvamcr72001@gmail.com>
2025-02-24 12:32:26 +0530

    CORS API CHANGES
    CORS API CHANGES

--- api/endpoints.py
+++ api/endpoints.py
@@ -1,10 +1,17 @@
 from fastapi import APIRouter
-from services.chatbot import process_query
+from fastapi.responses import JSONResponse
 from models.schemas import QueryRequest, QueryResponse
+from services.chatbot import process_query
 
 router = APIRouter()
 
 @router.post("/chat", response_model=QueryResponse)
 def chat(request: QueryRequest):
-    response_text = process_query(request.user_id,request.query)
-    return QueryResponse(response=response_text)
+    response_text = process_query(request.user_id, request.query)
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
+
+    return JSONResponse(content={"response": response_text}, headers=headers)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
-from api import endpoints
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="Mobile Chatbot API")
 
@@ -10,14 +10,21 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"], 
+    expose_headers=["Access-Control-Allow-Origin", "Access-Control-Allow-Headers"],
 )
 
+from api import endpoints
 app.include_router(endpoints.router)
 
 @app.get("/")
 def home():
-    return {"message": "Chatbot API is running!"}
+    headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
+    return JSONResponse(content={"message": "Chatbot API is running!"}, headers=headers)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
